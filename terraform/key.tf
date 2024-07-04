b2ebaadea490a6eaeccf9bcf20c87266974176b9
Pedro-Hashicorp <pedro.ortizdezuniga@hashicorp.com>
2024-07-04 12:10:34 +0200

    First commit of the project

---
+++ terraform/key.tf
@@ -0,0 +1,23 @@
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+data "aws_key_pair" "existing" {
+  key_name = "portiz"  # Replace with your key pair name
+}
+
+# Local variable to determine if the key pair exists
+locals {
+  key_pair_exists = length(data.aws_key_pair.existing.id) > 0
+}
+
+resource "aws_key_pair" "kp" {
+  count      = local.key_pair_exists ? 0 : 1
+  key_name   = "portiz"       # Create a "myKey" to AWS!!
+  public_key = tls_private_key.pk.public_key_openssh
+
+  provisioner "local-exec" { # Create a "myKey.pem" to your computer!!
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ./portiz.pem"
+  }
+}
---
+++ terraform/security_groups.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "my_sg" {
+  name        = "dev_sg"
+  description = "dev security group"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress{
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = [var.vpc_cidr]
+  }  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
