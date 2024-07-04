cc26b8b9c026364080e03fa55889395d717f8e74
Pedro-Hashicorp <pedro.ortizdezuniga@hashicorp.com>
2024-07-04 13:35:03 +0200

    outputs

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "instance_ips" {
   value = aws_instance.ec2_subnetA.private_ip
 }
-output "instance_ips" {
+output "instance_ips2" {
   value = aws_instance.ec2_subnetB.private_ip
 }
-output "instance_ips" {
+output "instance_ips3" {
   value = aws_instance.ec2_subnetC.private_ip
 }
 
