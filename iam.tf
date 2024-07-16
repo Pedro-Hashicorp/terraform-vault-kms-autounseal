0c0e543d34f175942d619af66d6d8e56a0dba957
Pedro-Hashicorp <pedro.ortizdezuniga@hashicorp.com>
2024-07-16 12:55:23 +0200

    asdasd

---
+++ iam.tf
@@ -0,0 +1,44 @@
+resource "aws_iam_role" "s3_access_role" {
+  name = "s3_access_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "s3_access_policy" {
+  name = "s3_access_policy"
+  role = aws_iam_role.s3_access_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ],
+        Effect   = "Allow",
+        Resource = [
+          "${aws_s3_bucket.my_bucket.arn}",
+          "${aws_s3_bucket.my_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "s3_access_instance_profile" {
+  name = "s3_access_instance_profile"
+  role = aws_iam_role.s3_access_role.name
+}
--- main.tf
+++ main.tf
@@ -14,15 +14,26 @@ resource "aws_route_table" "my_public_rt" {
   }
 }
 
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "pedroform-bucket"  # Replace with your unique bucket name
+  acl    = "private"
+
+  tags = {
+    Name        = "MyBucket"
+    Environment = "Dev"
+  }
+}
 
 resource "aws_instance" "ec2_node" {
-  count = 3
+  count = 1
   instance_type          = "t2.micro"
   ami                    = data.aws_ami.amazon_linux_2.id
   vpc_security_group_ids = [aws_security_group.my_sg.id]
   subnet_id              = data.aws_subnet.subnets[var.subnet_cidrs[count.index]].id
   key_name               = "portiz"
   private_ip             = var.private_ips[count.index]
+  iam_instance_profile = aws_iam_instance_profile.s3_access_instance_profile.name
+
 
   root_block_device {
     volume_size = 20
