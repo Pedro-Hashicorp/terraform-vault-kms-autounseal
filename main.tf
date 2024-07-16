486d9e6560470249e05fefd59cffeb6723fe0585
Pedro-Hashicorp <pedro.ortizdezuniga@hashicorp.com>
2024-07-16 14:15:20 +0200

    asda

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_s3_bucket" "my_bucket" {
   }
 }
 resource "aws_s3_bucket_acl" "s3_acl" {
-  bucket = aws_s3_bucket.my_bucket
+  bucket = aws_s3_bucket.my_bucket.id
   acl = "private"
 }
 
