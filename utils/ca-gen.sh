b9cc47811fa5204f6771b8309e86489a5af44917
Pedro-Hashicorp <pedro.ortizdezuniga@hashicorp.com>
2024-07-24 14:41:39 +0200

    gsdgdsf

---
+++ utils/ca-gen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set the CA certificate's validity period in days
+VALIDITY_DAYS=3
+
+# Create directory for the CA
+mkdir vault-ca
+
+# Generate a private key for the CA
+openssl genrsa -out vault-ca/vault-ca.key 2048
+
+# Create a self-signed CA certificate
+openssl req -x509 -new -nodes -key vault-ca/vault-ca.key -sha256 -days ${VALIDITY_DAYS} -out vault-ca/vault-ca.pem -subj "/C=US/ST=California/L=San Francisco/O=HashiCorp/OU=CDL/CN=HashiCorp Vault Self-signed CA"
