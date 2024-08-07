150e322eda80821e19ee29371f1185340b438e41
Pedro-Hashicorp <pedro.ortizdezuniga@hashicorp.com>
2024-08-07 12:11:15 +0200

    added lb url

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,11 @@ output "name" {
 
 output "urls" {
   value = [
-    for ip in aws_instance.ec2_node[*].public_ip : "http://${ip}:8200"
+    for ip in aws_instance.ec2_node[*].public_ip : "https://${ip}:8200"
   ]
+}
+
+output "lb_url" {
+  value = aws_lb.vault_lb.dns_name
+  
 }
