e74d562281c0c8c9bb43b5ec846e15dbfe3cfa2f
Pedro-Hashicorp <pedro.ortizdezuniga@hashicorp.com>
2024-09-09 10:44:20 +0200

    S3 bucket creation removed

--- iam.tf
+++ iam.tf
@@ -1,3 +1,47 @@
+resource "aws_iam_role" "s3_access_role" {
+  name = "s3_access_role_prueba"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+}
+
+resource "aws_iam_role_policy" "s3_access_policy" {
+  name = "s3_access_policy"
+  role = aws_iam_role.s3_access_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "s3:*"
+        ],
+        Effect   = "Allow",
+        Resource = [
+          "*"
+        ]
+      }
+    ]
+  })
+  
+}
+
+resource "aws_iam_role_policy_attachment" "vault_kms_attach" {
+  role       = aws_iam_role.s3_access_role.name
+  policy_arn = aws_iam_policy.kms_access.arn
+}
+
 resource "aws_iam_policy" "kms_access" {
   name = "kms"
 
@@ -21,4 +65,9 @@ resource "aws_iam_policy" "kms_access" {
   ]
 })
   
+}
+
+resource "aws_iam_instance_profile" "iam-for-ec2-create-s33" {
+  name = "iam-for-ec2-create-s33"
+  role = aws_iam_role.s3_access_role.name
 }
